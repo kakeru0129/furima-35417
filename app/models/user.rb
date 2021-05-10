6f924ed91442bd95f4f3220d44e0e5706f022e84
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-05-10 16:17:56 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birthday
+  end
+  
   validates :password, length: { minimum: 6 }
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+
   with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角文字を使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday, presence: true
-
-  has_many :items
-  has_many :orders
 
 end
