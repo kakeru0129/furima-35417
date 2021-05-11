82d3c80526dbdb30b5ae3589b7b7acacddd89aee
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-05-11 12:35:28 +0900

    商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,19 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:title, :desription, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
 end
