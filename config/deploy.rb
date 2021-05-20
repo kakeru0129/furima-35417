179291c211e8cd39498dfd05008fd1ffd73fbae6
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-05-20 16:09:05 +0900

    Capistranoの導入

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.16.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'furima-35417'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:kakeru0129/furima-35417.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/ka01ke29ru.pem ec2-user@18.177.200.201'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
